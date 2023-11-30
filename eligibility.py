c4c064921295d579b0beff1eb9d0e07c0c0ec13f
Daniele <busond5@gmail.com>
2023-11-30 18:39:49 +0100

    found several abstract for philpapers

--- eligibility.py
+++ eligibility.py
@@ -49,18 +49,89 @@ from datetime import datetime
 
 
 ## Springer case
-springer = pd.read_csv("springer2123N.csv")
+# springer = pd.read_csv("springer2123N.csv")
 
-print(len(springer['Abstract'][springer['Abstract'].isna()]))
+# print(len(springer['Abstract'][springer['Abstract'].isna()]))
+
+# i = 0
+# for i in range(len(springer['Abstract'])):
+    
+#     if isinstance(springer['Abstract'].loc[i], float):
+
+#         obj = datetime.now()
+
+#         url = springer['URL'].loc[i]
+#         print(i, ") ", '\x1b[6;30;42m' + url + '\x1b[0m')
+
+#         driver = webdriver.Chrome()
+#         driver.get(url)
+#         # driver.maximize_window()
+#         time.sleep(0.01)
+#         page = driver.page_source
+#         soup = BeautifulSoup(page, 'html.parser')
+
+#         main_content = soup.find('div', class_='main-content')
+
+#         if main_content:
+#             springer.loc[i, 'Abstract'] = ""
+#             for element in main_content.select("p"):
+                
+#                 springer.loc[i, 'Abstract'] += element.get_text()
+#                 springer.to_csv('springer2123N.csv', index=False)
+        
+#         abstracts_div = soup.find('div', id='Abs1-content')
+
+#         if abstracts_div:
+#             springer.loc[i, 'Abstract'] = ""
+#             for element in abstracts_div.select("p"):
+                
+#                 springer.loc[i, 'Abstract'] += element.get_text()
+#                 springer.to_csv('springer2123N.csv', index=False)
+
+#         bookSection = soup.find('div', class_='c-book-section')
+
+#         if bookSection:
+#             springer.loc[i, 'Abstract'] = ""
+#             if bookSection.select("p"):
+#                 for element in bookSection.select("p"):
+#                     springer.loc[i, 'Abstract'] += element.get_text()
+#                     springer.to_csv('springer2123N.csv', index=False)
+#             else:
+#                 springer.loc[i, "Abstract"] = element.get_text()
+        
+#         articleSection = soup.find('div', class_='c-article-section__content')
+
+#         if articleSection:
+#             for element in articleSection.select("p"):
+                
+#                 springer.loc[i, 'Abstract'] = element.get_text()
+#                 springer.to_csv('springer2123N.csv', index=False)
+
+        
+#     if isinstance(springer['Abstract'].loc[i], float) and isinstance(springer['Keywords'], float):
+#         print("book")
+        
+
+# driver.quit()
+# print(len(springer['Abstract'][springer['Abstract'].isna()]))
+
+# Save the updated DataFrame back to the CSV file
+# springer.to_csv('springer2123.csv', index=False)
+
+# Philpapers case
+philpapers = pd.read_csv("philpapers2123N.csv")
+
+print(len(philpapers['Abstract'][philpapers['Abstract'].isna()]))
 
 i = 0
-for i in range(len(springer['Abstract'])):
+for i in range(len(philpapers['Abstract'])):
     
-    if isinstance(springer['Abstract'].loc[i], float):
+    if isinstance(philpapers['Abstract'].loc[i], float):
 
+        print(philpapers.loc[i, 'URL'])
         obj = datetime.now()
 
-        url = springer['URL'].loc[i]
+        url = philpapers['URL'].loc[i]
         print(i, ") ", '\x1b[6;30;42m' + url + '\x1b[0m')
 
         driver = webdriver.Chrome()
@@ -70,50 +141,121 @@ for i in range(len(springer['Abstract'])):
         page = driver.page_source
         soup = BeautifulSoup(page, 'html.parser')
 
-        main_content = soup.find('div', class_='main-content')
+        # main_content = soup.find('div', class_='main-content')
 
-        if main_content:
-            springer.loc[i, 'Abstract'] = ""
-            for element in main_content.select("p"):
+        # if main_content:
+        #     philpapers.loc[i, 'Abstract'] = ""
+        #     for element in main_content.select("p"):
                 
-                springer.loc[i, 'Abstract'] += element.get_text()
-                springer.to_csv('springer2123N.csv', index=False)
+        #         philpapers.loc[i, 'Abstract'] += element.get_text()
+        #         philpapers.to_csv('philpapers2123N.csv', index=False)
         
-        abstracts_div = soup.find('div', id='Abs1-content')
+        # abstracts_div = soup.find('div', id='Abs1-content')
 
-        if abstracts_div:
-            springer.loc[i, 'Abstract'] = ""
-            for element in abstracts_div.select("p"):
+        # if abstracts_div:
+        #     philpapers.loc[i, 'Abstract'] = ""
+        #     for element in abstracts_div.select("p"):
                 
-                springer.loc[i, 'Abstract'] += element.get_text()
-                springer.to_csv('springer2123N.csv', index=False)
+        #         philpapers.loc[i, 'Abstract'] += element.get_text()
+        #         philpapers.to_csv('philpapers2123N.csv', index=False)
 
-        bookSection = soup.find('div', class_='c-book-section')
+        # bookSection = soup.find('div', class_='c-book-section')
 
-        if bookSection:
-            springer.loc[i, 'Abstract'] = ""
-            if bookSection.select("p"):
-                for element in bookSection.select("p"):
-                    springer.loc[i, 'Abstract'] += element.get_text()
-                    springer.to_csv('springer2123N.csv', index=False)
-            else:
-                springer.loc[i, "Abstract"] = element.get_text()
+        # if bookSection:
+        #     philpapers.loc[i, 'Abstract'] = ""
+        #     if bookSection.select("p"):
+        #         for element in bookSection.select("p"):
+        #             philpapers.loc[i, 'Abstract'] += element.get_text()
+        #             philpapers.to_csv('philpapers2123N.csv', index=False)
+        #     else:
+        #         philpapers.loc[i, "Abstract"] = element.get_text()
+
+        # abs001 = soup.find('div', class_='abstract author')
+
+        # if abs001:
+        #     philpapers.loc[i, 'Abstract'] = ""
+        #     print(abs001.get_text())
+        #     philpapers.loc[i, 'Abstract'] = abs001.get_text()
+        #     philpapers.to_csv('philpapers2123N.csv', index=False)
         
-        articleSection = soup.find('div', class_='c-article-section__content')
+        # articleSection = soup.find('div', class_='c-article-section__content')
 
-        if articleSection:
-            for element in articleSection.select("p"):
+        # if articleSection:
+        #     philpapers.loc[i, 'Abstract'] = ""
+        #     for element in articleSection.select("p"):
                 
-                springer.loc[i, 'Abstract'] = element.get_text()
-                springer.to_csv('springer2123N.csv', index=False)
+        #         philpapers.loc[i, 'Abstract'] += element.get_text()
+        #         philpapers.to_csv('philpapers2123N.csv', index=False)
+
+        # hlFld = soup.find('div', class_='hlFld-Abstract')
+
+        # if hlFld:
+        #     philpapers.loc[i, 'Abstract'] = ""
+        #     for element in hlFld.select("p"):
+        #         print(element.get_text())
+        #         philpapers.loc[i, 'Abstract'] += element.get_text()
+        #         philpapers.to_csv('philpapers2123N.csv', index=False)
+
+        article = soup.find('div', class_='article fulltext-view')
+
+        if article:
+            philpapers.loc[i, 'Abstract'] = ""
+            for element in article.select("p"):
+                print(element.get_text())
+                philpapers.loc[i, 'Abstract'] += element.get_text()
+                philpapers.to_csv('philpapers2123N.csv', index=False)
+
+        article = soup.find('div', class_='article extract-view')
+
+        if article:
+            philpapers.loc[i, 'Abstract'] = ""
+            for element in article.select("p"):
+                print(element.get_text())
+                philpapers.loc[i, 'Abstract'] += element.get_text()
+                philpapers.to_csv('philpapers2123N.csv', index=False)
+
+        article = soup.find('div', id='sec001')
+
+        if article:
+            philpapers.loc[i, 'Abstract'] = ""
+            for element in article.select("p"):
+                print(element.get_text())
+                philpapers.loc[i, 'Abstract'] += element.get_text()
+                philpapers.to_csv('philpapers2123N.csv', index=False)
+
+        article = soup.find('div', id='core-container')
+
+        if article:
+            philpapers.loc[i, 'Abstract'] = ""
+            for element in article.select("p"):
+                print(element.get_text())
+                philpapers.loc[i, 'Abstract'] += element.get_text()
+                philpapers.to_csv('philpapers2123N.csv', index=False)
+
+        article = soup.find('div', id='sec no-title')
+
+        if article:
+            philpapers.loc[i, 'Abstract'] = ""
+            for element in article.select("p"):
+                print(element.get_text())
+                philpapers.loc[i, 'Abstract'] += element.get_text()
+                philpapers.to_csv('philpapers2123N.csv', index=False)
+        
+        article = soup.find('div', id='article_abstract')
+
+        if article:
+            philpapers.loc[i, 'Abstract'] = ""
+            for element in article.select("p"):
+                print(element.get_text())
+                philpapers.loc[i, 'Abstract'] += element.get_text()
+                philpapers.to_csv('philpapers2123N.csv', index=False)
+
 
         
-    if isinstance(springer['Abstract'].loc[i], float) and isinstance(springer['Keywords'], float):
-        print("book")
+    # if isinstance(philpapers['Abstract'].loc[i], float) and isinstance(philpapers['Keywords'], float):
+    #     print("book")
         
 
 driver.quit()
-print(len(springer['Abstract'][springer['Abstract'].isna()]))
+print(len(philpapers['Abstract'][philpapers['Abstract'].isna()]))
 
-# Save the updated DataFrame back to the CSV file
-# springer.to_csv('springer2123.csv', index=False)
