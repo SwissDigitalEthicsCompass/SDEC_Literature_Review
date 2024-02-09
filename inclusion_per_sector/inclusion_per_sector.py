b8455d960ee81a1b1baba2598618bc215b105488
Daniele <busond5@gmail.com>
2024-02-09 16:55:01 +0100

    implemented a new way of checking categories

--- inclusion_per_sector/inclusion_per_sector.py
+++ inclusion_per_sector/inclusion_per_sector.py
@@ -29,9 +29,11 @@ def get_score(cleaned_string, keywords):
     score = count/total_count
     return score
 
-def get_count_library(library, keywords):
+def get_count_library(library, keywords, name):
     count = 0
     new_lib = pd.read_csv("manual_work/review.csv")
+    test_category = pd.DataFrame()
+    k = 0
     for i in library.index:
         
         score = 0
@@ -47,84 +49,129 @@ def get_count_library(library, keywords):
 
         if score > 0:
             count += 1
+            
             for col in library.columns:
                 j = len(new_lib.index) + 1
-                new_lib.loc[i, col] = library[col].loc[i]
+                new_lib.loc[j, col] = library[col].loc[i]
+                test_category.loc[k, col] = library[col].loc[i]
+            k += 1
 
     new_lib.to_csv(f"manual_work/review.csv")
+    test_category.to_csv(f"manual_work/{name}_test_category.csv")
     return count
 
 # keywords
 
 service_dict = {
+
     'smart-cities': 1,
+
+    'smart-city': 1,
+
     'healthcare': 1,
+
     'government': 1,
+
     'education': 1,
+
     'transportation': 1,
+
     'security': 1,
-    'finance': 1
+
+    'finance': 1,
+
+    'law': 1
+
 }
 
 technology_field_dict = {
-    'AI': 1,
+
+    'ai': 1,
+
+    'artificial': 1,
+
+    'intelligence': 1,
+
     'gaming': 1,
+
+    'social': 1,
+
     'media': 1,
+
     'blockchain': 1,
+
     'robotics': 1,
+
     'analytics': 1,
+
     'cybersecurity': 1
+
 }
 
 sector_dict = {
+
     'industrial': 1,
-    'NGO': 1,
+
+    'ngo': 1,
+
     'public': 1,
+
     'academic': 1,
+
     'private': 1,
+
     'non-profit': 1,
+
     'health': 1
+
 }
 
 reflections_analysis_dict = {
+
     'sustainability': 1,
+
     'privacy': 1,
+
     'responsibility': 1,
+
     'equity': 1,
+
     'transparency': 1,
+
     'regulation': 1
+
 }
 
 # ACM 
     
-print(f"\nACM - service count: {get_count_library(acm, service_dict)}")
-print(f"ACM - technology/field count: {get_count_library(acm, technology_field_dict)}")
-print(f"ACM - sector count: {get_count_library(acm, sector_dict)}")
-print(f"ACM - reflections/analysis count: {get_count_library(acm, reflections_analysis_dict)}")
+print(f"\nACM - service count: {get_count_library(acm, service_dict, 'acm')}")
+print(f"ACM - technology/field count: {get_count_library(acm, technology_field_dict, 'acm')}")
+print(f"ACM - sector count: {get_count_library(acm, sector_dict, 'acm')}")
+print(f"ACM - reflections/analysis count: {get_count_library(acm, reflections_analysis_dict, 'acm')}")
 
 
 # IEEE
 
-print(f"\nIEEE- service count: {get_count_library(ieee, service_dict)}")
-print(f"IEEE- technology/field count: {get_count_library(ieee, technology_field_dict)}")
-print(f"IEEE- sector count: {get_count_library(ieee, sector_dict)}")
-print(f"IEEE- reflections/analysis count: {get_count_library(ieee, reflections_analysis_dict)}")
+print(f"\nIEEE- service count: {get_count_library(ieee, service_dict, 'ieee')}")
+print(f"IEEE- technology/field count: {get_count_library(ieee, technology_field_dict, 'ieee')}")
+print(f"IEEE- sector count: {get_count_library(ieee, sector_dict, 'ieee')}")
+print(f"IEEE- reflections/analysis count: {get_count_library(ieee, reflections_analysis_dict, 'ieee')}")
 
 
 # Philpapers
 
-print(f"\nPhilpapers - service count: {get_count_library(philpapers, service_dict)}")
-print(f"Philpapers - technology/field count: {get_count_library(philpapers, technology_field_dict)}")
-print(f"Philpapers - sector count: {get_count_library(philpapers, sector_dict)}")
-print(f"Philpapers - reflections/analysis count: {get_count_library(philpapers, reflections_analysis_dict)}")
+print(f"\nPhilpapers - service count: {get_count_library(philpapers, service_dict, 'philpapers')}")
+print(f"Philpapers - technology/field count: {get_count_library(philpapers, technology_field_dict, 'philpapers')}")
+print(f"Philpapers - sector count: {get_count_library(philpapers, sector_dict, 'philpapers')}")
+print(f"Philpapers - reflections/analysis count: {get_count_library(philpapers, reflections_analysis_dict, 'philpapers')}")
 
 
 # Springer
 
-print(f"\nSpringer - service count: {get_count_library(springer, service_dict)}")
-print(f"Springer - technology/field count: {get_count_library(springer, technology_field_dict)}")
-print(f"Springer - sector count: {get_count_library(springer, sector_dict)}")
-print(f"Springer - reflections/analysis count: {get_count_library(springer, reflections_analysis_dict)}")
+print(f"\nSpringer - service count: {get_count_library(springer, service_dict, 'springer')}")
+print(f"Springer - technology/field count: {get_count_library(springer, technology_field_dict, 'springer')}")
+print(f"Springer - sector count: {get_count_library(springer, sector_dict, 'springer')}")
+print(f"Springer - reflections/analysis count: {get_count_library(springer, reflections_analysis_dict, 'springer')}")
 
 # cleaning the manual work review.csv
 
@@ -136,3 +183,27 @@ for col in review.columns:
         review = review.drop_duplicates(subset="DOI")
 
 review.to_csv("manual_work/review.csv")
+
+print()
+
+doi_list = review['DOI'].to_list()
+print(f"\nlength of doi list: {len(doi_list)}, length without duplicates: {len(set(doi_list))}")
+
+library_list = [acm, springer, philpapers, ieee]
+j = 0
+final_lib_manual = pd.DataFrame()
+for library in library_list:
+
+    for col in library.columns:
+        if col.startswith("Unnamed"):
+            library = library.drop(col, axis=1)
+
+    for i in library.index:
+        if not library['DOI'].loc[i] in doi_list:
+            for col in library.columns:
+                final_lib_manual.loc[j, col] = library[col].loc[i]
+            j += 1
+        
+
+print(final_lib_manual)
+final_lib_manual.to_csv("manual_work/final_review.csv")
